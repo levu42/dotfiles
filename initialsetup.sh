14ae5c5bf238ac2e6c6c01427f2e29fee7d77ef7
levu <levu@alderaan.scienceco.de>
2012-06-17 00:09:58 +0200

    updates

--- initialsetup.sh
+++ initialsetup.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
+echo ""
 echo "installing ssh and git:"
 sudo apt-get install ssh git-core
 
+echo ""
 echo "generating ssh-key:"
 ssh-keygen -t rsa -f "$HOME/.ssh/id_rsa.pub"
 
+echo ""
 echo "initializes git:"
 echo -n "git name? "
 read name
 echo -n "git mail? "
 read mail
-git set --global user.name $name
-git set --global user.email $mail
+git config --global user.name $name
+git config --global user.email $mail
 
-git clone git://github.com/levu42/dotfiles.git .
+echo ""
+echo ""
+
+git init
+git remote add origin git://github.com/levu42/dotfiles.git
+git pull origin
 
 rm initialsetup.sh
